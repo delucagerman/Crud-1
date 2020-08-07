06a0198f9d94a121713218b8d1787e5d74e3a94c
German <german@MacBook-Air-de-Apple.local>
2020-08-07 19:15:37 -0300

    con bootstrap

--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -2,9 +2,15 @@
   <div>
     {{ tarea.id }} - {{ tarea.nombre }}
     <h1>Editar</h1>
-    <form @submit.prevent="editarTarea(tarea)">
-      <input type="text" v-model="tarea.nombre" />
-      <button type="submit">Editar</button>
+    <form @submit.prevent="editarTarea(tarea)" class="form-inline">
+      <div class="input-group mb-2">
+        <div class="input-group-prepend">
+          <div class="input-group-text">Nombre</div>
+        </div>
+        <input type="text" v-model="tarea.nombre" class="form-control" />
+      </div>
+
+      <button type="submit" class="btn btn-primary mb-2 ml-2">Editar</button>
     </form>
   </div>
 </template>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <h1>Inicio</h1>
+    <h1>Lista de Tareas</h1>
     <router-link to="/agregar">
-      <button>Agregar</button>
+      <button class="btn btn-success btn-block">Agregar</button>
     </router-link>
-    <ul>
-      <li v-for="(item, index) in tareas" :key="index">
+    <ul class="list-group mt-5">
+      <li class="list-group-item" v-for="(item, index) in tareas" :key="index">
         {{ item.nombre }} - {{ item.id }}
-        <router-link :to="{name: 'Editar', params: {id: item.id}}">
-          <button>Editar</button>
-        </router-link>
-        <button @click="eliminarTarea(item.id)">Eliminar</button>
+        <div class="float-right">
+          <router-link :to="{name: 'Editar', params: {id: item.id}}">
+            <button class="btn btn-warning btn-sm mr-2">Editar</button>
+          </router-link>
+          <button class="btn btn-danger btn-sm" @click="eliminarTarea(item.id)">Eliminar</button>
+        </div>
       </li>
     </ul>
   </div>
